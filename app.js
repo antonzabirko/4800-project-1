2893e9f4a276dec6bb62df05d45089222398e1e7
Anton Zabirko <antonzabirko@outlook.com>
2020-03-08 11:39:52 -0400

    added webpage

--- app.js
+++
@@ -1,8 +0,0 @@
-import {Snake} from "gym-js";
-const env = new Snake();
-
-console.log(env.action_space.toString());
-console.log(env.observation_space.toString());
-
-let action = env.action_space.sample();
-let [obs, rew, done, info] = env.step(action);
